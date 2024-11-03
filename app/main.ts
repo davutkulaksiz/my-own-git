61d662eb9283fdf3bac55ad30f6008feeb09827e
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2024-11-03 16:53:55 +0300

    feat(my-own-git): implement `cat-file`

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,12 @@
 import * as fs from "fs";
+import zlib from "zlib";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
   Init = "init",
+  Catfile = "cat-file",
 }
 
 switch (command) {
@@ -15,6 +17,24 @@ switch (command) {
     fs.mkdirSync(".git/objects", { recursive: true });
     fs.mkdirSync(".git/refs", { recursive: true });
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
+    break;
+  case Commands.Catfile:
+    {
+      const flag = args[1];
+
+      if (flag === "-p") {
+        const blob = fs.readFileSync(
+          `.git/objects/${args[2].substring(0, 2)}/${args[2].substring(2)}`
+        );
+        const decompressedBlob = zlib.unzipSync(blob);
+        const content = decompressedBlob.subarray(
+          decompressedBlob.indexOf(0) + 1
+        );
+        process.stdout.write(content.toString());
+      } else {
+        throw new Error(`Unknown flag ${flag}`);
+      }
+    }
     break;
   default:
     throw new Error(`Unknown command ${command}`);
